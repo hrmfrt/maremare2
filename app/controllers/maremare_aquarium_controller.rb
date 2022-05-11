9ca893e600245a1ce7e2b5fc0aea0372de5ccd45
hrmfrt <hrmfrt@gmail.com>
2022-05-11 23:17:35 +0900

    Ajax機能 ボタンと生き物の画像まで

--- app/controllers/maremare_aquarium_controller.rb
+++ app/controllers/maremare_aquarium_controller.rb
@@ -1,20 +1,21 @@
 class MaremareAquariumController < ApplicationController
 
   def top
+    @categories = Category.all
   end
 
   def column
   end
 
   def information
-    @ponyo = Creature.find(6)
+
   end
 
   def writer
   end
 
   def dictionary
-    @ponyo = Creature.find(7)
+    @category = Category.find(params[:id])
   end
 
 
